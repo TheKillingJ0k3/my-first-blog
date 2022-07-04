3ab47a4607c39ea416867fca966bebb423ee129b
TheKillingJ0k3 <angry_sephiroth@hotmail.com>
2022-07-04 11:11:51 +0300

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),  # assign a view called post_list to root URL - if someone enters website, gets redirected to views.post_list
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
